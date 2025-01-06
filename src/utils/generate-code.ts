b4e3aeddef101831a7e3b4162430cc674982f08d
maarcos4g <marcospaullo552@gmail.com>
2025-01-06 16:30:20 -0300

    fix: change otp lenght to 6

--- src/utils/generate-code.ts
+++ src/utils/generate-code.ts
@@ -1,7 +1,7 @@
 export function generateCode(): string {
   const uniqueNumbers = new Set<number>();
 
-  while (uniqueNumbers.size < 4) {
+  while (uniqueNumbers.size < 6) {
       const randomNum = Math.floor(Math.random() * 10); // Random number between 0 and 9
       uniqueNumbers.add(randomNum);
   }
